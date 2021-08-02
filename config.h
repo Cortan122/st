db236b2fe9157ed73886e665c69b779214227cdd
Cortan122 <Cortan122@gmail.com>
2021-08-03 00:13:22 +0300

    dpi scaling and subpixel rendering

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mono:size=12:rgba=rgb:antialias=true:autohint=true";
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true",
-	"Unifont:pixelsize=16:antialias=flase"
+	"Inconsolata for Powerline:size=12:rgba=rgb:antialias=true:autohint=true",
+	"Unifont:size=12:rgba=rgb:antialias=flase"
 };
 static int borderpx = 2;
 
